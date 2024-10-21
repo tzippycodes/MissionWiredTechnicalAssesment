0735c7c9d50eda734a4af7450e4da37914d17f19
Anonymous <anon@example.com>
2024-10-21 10:25:36 -0400

    Begin creation of ETL Script
    Created MW_ETL.py in a git repository with comments outlining the goals of the exercise for readability.

---
+++ MW_ETL.py
@@ -0,0 +1,1 @@
+# A Python script to convert extract, transform, and load CRM data into "people" and "acquisition_facts" formatted using the assigned schema. Written for the MissionWired technical assessment for the Associate Data Engineer Role.
