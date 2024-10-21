1cf72a9b8d002838eaab6fc20dab4f8cbec6204e
Anonymous <anon@example.com>
2024-10-21 10:20:19 -0400

    Cleaned up code for distribution
    Enhanced readability, removed unnecessary parts of code, formatted for easier reuse with different csvs moving forward.

--- MW_ETL.py
+++ MW_ETL.py
@@ -1,7 +1,6 @@
 # A Python script to convert extract, transform, and load CRM data into "people" and "acquisition_facts" formatted using the assigned schema. Written for the MissionWired technical assessment for the Associate Data Engineer Role.
 
 # Importing the Required Dependencies
-
 import requests
 import pandas as pd
 from datetime import datetime
@@ -9,8 +8,19 @@ import logging as log_file
 from pathlib import Path
 from collections import OrderedDict
 
-# Defining the Logging Function
+# Defining variables to be used during function calls
+
+# CRM data from S3 buckets
+Constituent_Information = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv"
+Constituent_Email_Addresses = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv"
+Constituent_Subscription_Status = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv"
+
+# Local destination file paths
+people_file_path = "people.csv"
+acquisition_facts_file_path = "acquisition_facts.csv"
+
 def log_progress(message):
+    # Log the given message to the console and to code_log.txt, with a timestamp.
     timestamp_format = '%Y-%h-%d-%H:%M:%S' #Year-Monthname-Day-Hour-Minute-Second
     now = datetime.now() # get current timestamp
     timestamp = now.strftime(timestamp_format)
@@ -18,101 +28,83 @@ def log_progress(message):
         f.write(timestamp + ', ' + message + '\n')
         print(timestamp + ', ' + message + '\n')
 
-# Defining the extract function to retrieve the CSVs from the provided S3 Bucket to Pandas Dataframes
-
 def extract(url, file_name):
-    # Send an HTTP GET request to the S3 Bucket to extract data and save it as a csv
+    # Retrieve the CSVs from the provided S3 Bucket to Pandas Dataframes
+    # Sending an HTTP GET request to the S3 Bucket to extract data and save it as a csv
     response = requests.get(url)
 
+    # Name the export file
     export_file_name = file_name + ".csv"
 
+    # Write the extracted file to a local csv
     export_file_csv = open(export_file_name, "w")
     export_file_csv.write(response.text)
     export_file_csv.close()
 
-    # Reads the extracted csv to a Pandas Dataframe for transformation
+    # Read the extracted csv to a Pandas Dataframe for transformation
     df = pd.read_csv(export_file_name)
 
     return df
 
-# Defining the function to transform the dataframes in the desired schema
 def transform(df1, df2, df3):
-    # Create the dataframes that will be used combine the information from each of dataframes that hold the extracted data
+    # Create the new DataFrames that will be used combine the information from each of the initial DataFrames that hold the extracted data
     people_df = pd.DataFrame()
     acquisition_facts_df = pd.DataFrame()
 
-    # Change the necessary columns from each of the dataframes to the people DataFrame. Remove other columns.
+    # Change the necessary columns from each of the DataFrames to the people DataFrame.
     people_df['email'] = df2['email']
     people_df['code'] = df1['source']
     people_df['is_unsub'] = df3['isunsub']
     people_df['created_dt'] = df1['create_dt']
     people_df['updated_dt'] = df1['modified_dt']
 
-    # Cast datetime to date to obtain desired data type
+    # Cast datetime to date to obtain desired data type for acquisition_facts
     df1['create_dt'] = pd.to_datetime(df1['create_dt'])
     df1['create_dt'] = df1['create_dt'].map(lambda dt: dt.strftime("%m/%d/%Y"), na_action='ignore')
 
-    # Created ordered dictionary to count occurences of each date for 'aquisitions' column
+    # Create ordered dictionary to count occurences of each date for 'aquisitions' column
     counts = OrderedDict()
     for date in df1['create_dt']:
         counts[date] = counts.get(date, 0) + 1
     acquisition_facts_df['acquisition_date'] = list(counts.keys())
     acquisition_facts_df['acquisitions'] = list(counts.values())
+
     return people_df, acquisition_facts_df
 
-# Defining the load function to convert the transformed dataframe into a csv for distribution
-#def load(df, file_path):
-#    Path(file_path).parent.mkdir(parents=True, exist_ok=True)  
-#    df.to_csv(str(file_path), index=False) 
-
-    #return .csv
-
-# Defining variables to be used during function calls
-
-Constituent_Information = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv"
-Constituent_Email_Addresses = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv"
-Constituent_Subscription_Status = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv"
+def write_to_csv(df, file_path): 
+    # Convert the transformed dataframe into a csv for distribution
+    df.to_csv(file_path, header=True) 
 
 # Calling the defined functions to execute the ETL Process
 
 log_progress('Beginning ETL Process')
 
 Constituent_Information_df = extract(Constituent_Information, "Constituent_Information")
+
 log_progress('Constituent Information converted to Dataframe')
 
 Constituent_Email_Addresses_df= extract(Constituent_Email_Addresses, "Constituent_Email_Addresses")
+
 log_progress('Constituent Email Addresses converted to Dataframe')
 
 Constituent_Subscription_Status_df = extract(Constituent_Subscription_Status, "Constituent_Subscription_Status")
+
 log_progress('Constituent Subscription Status converted to Dataframe')
-
-print("DataFrame Name is: Constitutent_Information_df")
-print(Constituent_Information_df)
-
-print("DataFrame Name is: Constituent_Email_Addresses_df")
-print(Constituent_Email_Addresses_df)
-
-print("DataFrame Name is: Constitutent_Subscription_Status_df")
-print(Constituent_Subscription_Status_df)
 
 log_progress('Beginning Data Transformation')
 
 people_df, acquisition_facts_df = transform(Constituent_Information_df, Constituent_Email_Addresses_df, Constituent_Subscription_Status_df)
 
-print("Dataframe Name is: people_df")
-print(people_df)
+log_progress('Data Transformation Complete')
 
-print("Dataframe Name is: acquisition_facts_df")
-print(acquisition_facts_df)
+log_progress('Loading data to destination files')
 
-#log_progress('Data Transformation Complete')
+write_to_csv(people_df, people_file_path)
 
-#log_progress('Loading data to destination files')
+log_progress('File "people.csv" created.')
 
+write_to_csv(acquisition_facts_df, acquisition_facts_file_path)
 
+log_progress('File "acquisition_facts.csv" created.')
 
-people_df.to_csv("/mnt/Tzippy/Documents/Career/MissionWired/people.csv", header=True)
-
-acquisition_facts_df.to_csv("/mnt/Tzippy/Documents/Career/MissionWired/acquisition_facts.csv", header=True)
-
-#log_progress('Data loaded. Extract, Transform, and Load process complete.')
+log_progress('Data loaded. Extract, Transform, and Load process complete.')
