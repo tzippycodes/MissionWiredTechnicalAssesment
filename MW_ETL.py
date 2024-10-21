0ff2e308eb04330d32aeac5957e2b8567fa2809a
Anonymous <anon@example.com>
2024-10-21 10:20:18 -0400

    Building and Troubleshooting Transform Function
    Created more streamlined logging feature that prints in the terminal as well as the log file. Built Python code to change column names from CRM data to desired schema. Edited pd.read_csv statement to parse for datetime

--- MW_ETL.py
+++ MW_ETL.py
@@ -13,7 +13,8 @@ def log_progress(message):
     now = datetime.now() # get current timestamp
     timestamp = now.strftime(timestamp_format)
     with open("code_log.txt", "a") as f:
-        f.write(timestamp + ',' + message + '\n')
+        f.write(timestamp + ', ' + message + '\n')
+        print(timestamp + ', ' + message + '\n')
 
 # Defining the extract function to retrieve the CSVs from the provided S3 Bucket to Pandas Dataframes
 
@@ -30,11 +31,28 @@ def extract(url, file_name):
     export_file_csv.close()
 
     # Reads the extracted csv to a Pandas Dataframe for transformation
-    df = pd.read_csv(export_file_name)
+    df = pd.read_csv(export_file_name, parse_dates=True)
 
     return df
 
-# Defining the transform function 
+# Defining the transform function
+def transform(df1, df2, df3):
+    # Create the dataframes that will be used combine the information from each of dataframes that hold the extracted data
+    people_df = pd.DataFrame()
+    acquisition_facts_df = pd.DataFrame()
+
+    # Append only the necessary columns from each of the dataframes to the two dataframes that will be saved for export
+    people_df['code'] = df1['source']
+    people_df['created_dt'] = df1['create_dt']
+    people_df['email'] = df2['email']
+    people_df['is_unsub'] = df3['isunsub']
+
+    acquisition_facts_df['acquisition_date'] = people_df['created_dt']
+    acquisition_facts_df = pd.to_datetime(acquisition_facts_df['acquisition date'])
+    
+    # Assign the specified data types to the columns in both of the new dataframes
+
+    return df1, df2
 
 # Defining the load function
 
@@ -49,20 +67,27 @@ Constituent_Subscription_Status = "https://als-hiring.s3.amazonaws.com/fake_data
 
 log_progress('Beginning ETL Process')
 
-df = extract(Constituent_Information, "Constituent_Information")
+Constituent_Information_df = extract(Constituent_Information, "Constituent_Information")
 log_progress('Constituent Information converted to Dataframe')
 
-df2 = extract(Constituent_Email_Addresses, "Constituent_Email_Addresses")
+print(Constituent_Information_df['create_dt'])
+
+Constituent_Email_Addresses_df= extract(Constituent_Email_Addresses, "Constituent_Email_Addresses")
 log_progress('Constituent Email Addresses converted to Dataframe')
 
-df3 = extract(Constituent_Subscription_Status, "Constituent_Subscription_Status")
+Constituent_Subscription_Status_df = extract(Constituent_Subscription_Status, "Constituent_Subscription_Status")
 log_progress('Constituent Subscription Status converted to Dataframe')
 
-print(df.head)
-print(df2.head)
-print (df3.head)
+log_progress('Beginning Data Transformation')
 
-#log_progress('Beginning Data Transformation')
+people_df, acquisition_facts_df = transform(Constituent_Information_df, Constituent_Email_Addresses_df, Constituent_Subscription_Status_df)
+
+print(people_df.head, acquisition_facts_df.head)
+
+
+
+
+
 
 #log_progress('Data Transformation Complete')
 
