c059d2b32f4fa5fe64bf58025fd429cba16ddc31
Anonymous <anon@example.com>
2024-10-21 10:20:14 -0400

    Revised Approach with Greater Structure
    Shifted to requests library instead of pyfetch for ease of readability. Laid out remainder of comments of anticipated structure to guide remaining work

--- MW_ETL.py
+++ MW_ETL.py
@@ -1,6 +1,34 @@
 # A Python script to convert extract, transform, and load CRM data into "people" and "acquisition_facts" formatted using the assigned schema. Written for the MissionWired technical assessment for the Associate Data Engineer Role.
 
-#Importing the Required Dependencies
+# Importing the Required Dependencies
+
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
+
+# Defining the extract function to retrieve the CSVs from the provided S3 Bucket to Pandas Dataframes
+
+def extract(url):
+    # Send an HTTP GET request to the S3 Bucket to extract the csv
+    response = request.get(url)
+
+    #Store the extracted csv in a variable
+    csv = response.body
+
+    # Reads the extracted csv to a Pandas Dataframe for transformation
+    df = pd.read_csv(csv)
+
+    return df
+
+# Defining the transform function 
+
+# Defining the load function
+
+# Defining variables to be used during function calls
+
+Constituent_Information = https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv
+Constituent_Email_Addresses = https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv
+Constituent_Subscription_Status = https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv
+
+
+
+# Calling the defined functions to execute the ETL Process
