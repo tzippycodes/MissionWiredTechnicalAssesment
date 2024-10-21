c8db03b7cebf4df07d6496427b571b5ca7fb1a2e
Anonymous <anon@example.com>
2024-10-21 10:20:17 -0400

    Logging function and Extract  Troubleshooting
    Added logging function for ease of tracking script progress. Used terminal to troubleshoot extract function until csvs were extracted properly to dataframes. Onto transforming

--- MW_ETL.py
+++ MW_ETL.py
@@ -4,18 +4,33 @@
 
 import requests
 import pandas as pd
+from datetime import datetime
+import logging as log_file
+
+# Defining the Logging Function
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' #Year-Monthname-Day-Hour-Minute-Second
+    now = datetime.now() # get current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("code_log.txt", "a") as f:
+        f.write(timestamp + ',' + message + '\n')
 
 # Defining the extract function to retrieve the CSVs from the provided S3 Bucket to Pandas Dataframes
 
-def extract(url):
-    # Send an HTTP GET request to the S3 Bucket to extract the csv
-    response = request.get(url)
+def extract(url, file_name):
+    # Send an HTTP GET request to the S3 Bucket to extract data and save it as a csv
+    response = requests.get (url)
 
-    #Store the extracted csv in a variable
-    csv = response.body
+    export_file_name = file_name + ".csv"
+
+    export_file_csv = open(export_file_name, "w")
+
+    export_file_csv.write(response.text)
+
+    export_file_csv.close()
 
     # Reads the extracted csv to a Pandas Dataframe for transformation
-    df = pd.read_csv(csv)
+    df = pd.read_csv(export_file_name)
 
     return df
 
@@ -25,10 +40,32 @@ def extract(url):
 
 # Defining variables to be used during function calls
 
-Constituent_Information = https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv
-Constituent_Email_Addresses = https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv
-Constituent_Subscription_Status = https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv
-
+Constituent_Information = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv"
+Constituent_Email_Addresses = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv"
+Constituent_Subscription_Status = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv"
 
 
 # Calling the defined functions to execute the ETL Process
+
+log_progress('Beginning ETL Process')
+
+df = extract(Constituent_Information, "Constituent_Information")
+log_progress('Constituent Information converted to Dataframe')
+
+df2 = extract(Constituent_Email_Addresses, "Constituent_Email_Addresses")
+log_progress('Constituent Email Addresses converted to Dataframe')
+
+df3 = extract(Constituent_Subscription_Status, "Constituent_Subscription_Status")
+log_progress('Constituent Subscription Status converted to Dataframe')
+
+print(df.head)
+print(df2.head)
+print (df3.head)
+
+#log_progress('Beginning Data Transformation')
+
+#log_progress('Data Transformation Complete')
+
+#log_progress('Loading data to destination files')
+
+#log_progress('Data loaded. Extract, Transform, and Load process complete.')
