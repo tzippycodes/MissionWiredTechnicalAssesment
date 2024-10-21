335527b89128fe9c0145c5e568d27c90d636237c
Anonymous <anon@example.com>
2024-10-21 10:20:14 -0400

    Importing Required Dependencies
    Importing the dependencies that are most likely  needed - may add more or change based on need later as necessary

--- MW_ETL.py
+++ MW_ETL.py
@@ -1,1 +1,6 @@
 # A Python script to convert extract, transform, and load CRM data into "people" and "acquisition_facts" formatted using the assigned schema. Written for the MissionWired technical assessment for the Associate Data Engineer Role.
+
+#Importing the Required Dependencies
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
