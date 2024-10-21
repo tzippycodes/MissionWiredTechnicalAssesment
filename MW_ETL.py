73522a10f15afa67f363b10ed404b4ed72c7a71a
Anonymous <anon@example.com>
2024-10-21 10:20:18 -0400

    Fixed Transform function
    Count occurences of each date for acquisitions column

--- MW_ETL.py
+++ MW_ETL.py
@@ -6,7 +6,8 @@ import requests
 import pandas as pd
 from datetime import datetime
 import logging as log_file
-from pathlib import Path 
+from pathlib import Path
+from collections import OrderedDict
 
 # Defining the Logging Function
 def log_progress(message):
@@ -38,24 +39,25 @@ def extract(url, file_name):
 def transform(df1, df2, df3):
     # Create the dataframes that will be used combine the information from each of dataframes that hold the extracted data
     people_df = pd.DataFrame()
-    acquisition_facts_df = pd.DataFrame(['acquisition_facts','acquisitions'])
+    acquisition_facts_df = pd.DataFrame()
 
-    # Change the necessary columns from each of the dataframes to the initial dataframe. Remove other columns.
-    people_df['email'] = df2['email'].astype(str)
-    people_df['code'] = df1['source'].astype(str)
-    people_df['is_unsub'] = df3['isunsub'].astype(bool)
+    # Change the necessary columns from each of the dataframes to the people DataFrame. Remove other columns.
+    people_df['email'] = df2['email']
+    people_df['code'] = df1['source']
+    people_df['is_unsub'] = df3['isunsub']
     people_df['created_dt'] = df1['create_dt']
     people_df['updated_dt'] = df1['modified_dt']
 
-    acquisition_facts_df['acquisition_date'] = pd.to_datetime(df1['create_dt'])
-    acquisition_facts_df ['acquisition_date'] = acquisition_facts_df['acquisition_date'].map(lambda dt: dt.strftime("%m/%d/%Y"))
-    acquisition_facts_df['acquisitions'] = acquisition_facts_df['acquisition_date'].value_counts() 
+    # Cast datetime to date to obtain desired data type
+    df1['create_dt'] = pd.to_datetime(df1['create_dt'])
+    df1['create_dt'] = df1['create_dt'].map(lambda dt: dt.strftime("%m/%d/%Y"), na_action='ignore')
 
-
-    # Assign the specified data types to the columns in both of the new dataframes
-    # change the datetime format
-    #df['date_formatted'] = df['date'].dt.strftime('/ %H:%M:%S')
-
+    # Created ordered dictionary to count occurences of each date for 'aquisitions' column
+    counts = OrderedDict()
+    for date in df1['create_dt']:
+        counts[date] = counts.get(date, 0) + 1
+    acquisition_facts_df['acquisition_date'] = list(counts.keys())
+    acquisition_facts_df['acquisitions'] = list(counts.values())
     return people_df, acquisition_facts_df
 
 # Defining the load function to convert the transformed dataframe into a csv for distribution
@@ -71,9 +73,6 @@ Constituent_Information = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07
 Constituent_Email_Addresses = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv"
 Constituent_Subscription_Status = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv"
 
-people_csv_file_path = "/mnt/Tzippy/Documents/Career/MissionWired/people.csv"
-acquisition_facts_csv_file_path = "/mnt/Tzippy/Documents/Career/MissionWired/acquisition_facts.csv"
-
 # Calling the defined functions to execute the ETL Process
 
 log_progress('Beginning ETL Process')
@@ -81,13 +80,20 @@ log_progress('Beginning ETL Process')
 Constituent_Information_df = extract(Constituent_Information, "Constituent_Information")
 log_progress('Constituent Information converted to Dataframe')
 
-print(Constituent_Information_df['create_dt'])
-
 Constituent_Email_Addresses_df= extract(Constituent_Email_Addresses, "Constituent_Email_Addresses")
 log_progress('Constituent Email Addresses converted to Dataframe')
 
 Constituent_Subscription_Status_df = extract(Constituent_Subscription_Status, "Constituent_Subscription_Status")
 log_progress('Constituent Subscription Status converted to Dataframe')
+
+print("DataFrame Name is: Constitutent_Information_df")
+print(Constituent_Information_df)
+
+print("DataFrame Name is: Constituent_Email_Addresses_df")
+print(Constituent_Email_Addresses_df)
+
+print("DataFrame Name is: Constitutent_Subscription_Status_df")
+print(Constituent_Subscription_Status_df)
 
 log_progress('Beginning Data Transformation')
 
