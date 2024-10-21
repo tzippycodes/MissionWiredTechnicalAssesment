c9fb081e1c96b63fd103131294d63e0d16eb1f93
Anonymous <anon@example.com>
2024-10-21 10:20:18 -0400

    Fixed DataFrame and CSV transform and Load
    Fixed errors in transform function to return both DataFrames, created load feature, will define in function and finalize data typing for both DataFrames and create transformations for acuisition_facts_df

--- MW_ETL.py
+++ MW_ETL.py
@@ -6,6 +6,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 import logging as log_file
+from pathlib import Path 
 
 # Defining the Logging Function
 def log_progress(message):
@@ -20,14 +21,12 @@ def log_progress(message):
 
 def extract(url, file_name):
     # Send an HTTP GET request to the S3 Bucket to extract data and save it as a csv
-    response = requests.get (url)
+    response = requests.get(url)
 
     export_file_name = file_name + ".csv"
 
     export_file_csv = open(export_file_name, "w")
-
     export_file_csv.write(response.text)
-
     export_file_csv.close()
 
     # Reads the extracted csv to a Pandas Dataframe for transformation
@@ -35,26 +34,39 @@ def extract(url, file_name):
 
     return df
 
-# Defining the transform function
+# Defining the function to transform the dataframes in the desired schema
 def transform(df1, df2, df3):
     # Create the dataframes that will be used combine the information from each of dataframes that hold the extracted data
     people_df = pd.DataFrame()
-    acquisition_facts_df = pd.DataFrame()
+    acquisition_facts_df = pd.DataFrame(['acquisition_facts','acquisitions'])
 
-    # Append only the necessary columns from each of the dataframes to the two dataframes that will be saved for export
-    people_df['code'] = df1['source']
-    people_df['created_dt'] = df1['create_dt']
+    # Change the necessary columns from each of the dataframes to the initial dataframe. Remove other columns.
     people_df['email'] = df2['email']
+    people_df['code'] = df1['source']
     people_df['is_unsub'] = df3['isunsub']
+    people_df['created_dt'] = df1['create_dt']
+    people_df['updated_dt'] = df1['modified_dt']
+    people_df = people_df[['email', 'code', 'is_unsub', 'created_dt', 'updated_dt']]
 
-    acquisition_facts_df['acquisition_date'] = people_df['created_dt']
-    acquisition_facts_df = pd.to_datetime(acquisition_facts_df['acquisition date'])
-    
+    acquisition_facts_df['acquisition_date'] = df1['create_dt']
+    acquisition_facts_df ['acquisition_date'] = str(pd.to_datetime(acquisition_facts_df['acquisition_date'], dayfirst=True))
+    #acquisitions = {(acquisition_facts_df['acquisition_date'].value_counts())}
+    #acquisition_facts_df['acquisitions'] = acquisitions 
+    acquisition_facts_df = acquisition_facts_df[['acquisition_date']]
+
+
     # Assign the specified data types to the columns in both of the new dataframes
+    # change the datetime format
+    #df['date_formatted'] = df['date'].dt.strftime('%Y/%m/%d %H:%M:%S')
 
-    return df1, df2
+    return people_df, acquisition_facts_df
 
-# Defining the load function
+# Defining the load function to convert the transformed dataframe into a csv for distribution
+#def load(df, file_path):
+#    Path(file_path).parent.mkdir(parents=True, exist_ok=True)  
+#    df.to_csv(str(file_path), index=False) 
+
+    return csv
 
 # Defining variables to be used during function calls
 
@@ -62,6 +74,8 @@ Constituent_Information = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07
 Constituent_Email_Addresses = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv"
 Constituent_Subscription_Status = "https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv"
 
+people_csv_file_path = "/mnt/Tzippy/Documents/Career/MissionWired/people.csv"
+acquisition_facts_csv_file_path = "/mnt/Tzippy/Documents/Career/MissionWired/acquisition_facts.csv"
 
 # Calling the defined functions to execute the ETL Process
 
@@ -82,15 +96,20 @@ log_progress('Beginning Data Transformation')
 
 people_df, acquisition_facts_df = transform(Constituent_Information_df, Constituent_Email_Addresses_df, Constituent_Subscription_Status_df)
 
-print(people_df.head, acquisition_facts_df.head)
+print("Dataframe Name is: people_df")
+print(people_df)
 
-
-
-
-
+print("Dataframe Name is: acquisition_facts_df")
+print(acquisition_facts_df)
 
 #log_progress('Data Transformation Complete')
 
 #log_progress('Loading data to destination files')
+
+
+
+people_df.to_csv("/mnt/Tzippy/Documents/Career/MissionWired/people.csv", header=True)
+
+acquisition_facts_df.to_csv("/mnt/Tzippy/Documents/Career/MissionWired/acquisition_facts.csv", header=True)
 
 #log_progress('Data loaded. Extract, Transform, and Load process complete.')
